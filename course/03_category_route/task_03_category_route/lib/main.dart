7e867d061d358af5b96403cd51fd68d23cbd1000
JP Hashagen <jayp33e@gmail.com>
2018-05-26 19:21:52 +0200

    Import the CategoryRoute widget

--- course/03_category_route/task_03_category_route/lib/main.dart
+++ course/03_category_route/task_03_category_route/lib/main.dart
@@ -5,7 +5,7 @@
 // You can read about packages here: https://flutter.io/using-packages/
 import 'package:flutter/material.dart';
 
-// TODO: Import the CategoryRoute widget
+import 'category_route.dart';
 
 /// The function that is called when main.dart is run.
 void main() {
